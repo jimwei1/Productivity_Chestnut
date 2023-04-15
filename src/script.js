d138630fc0be17ee90ccdb4608040ddf20cfadb5
Thomas Li <licxb@bc.edu>
2023-04-15 13:41:37 -0500

    initial

---
+++ src/script.js
@@ -0,0 +1,24 @@
+let duration = 5 * 60; // 5 minutes in seconds
+let timeLeft = duration;
+let timerInterval = null;
+
+function updateTimer() {
+  const minutes = Math.floor(timeLeft / 60).toString().padStart(2, "0");
+  const seconds = (timeLeft % 60).toString().padStart(2, "0");
+  const timerString = `${minutes}:${seconds}`;
+  document.getElementById("timer").textContent = timerString;
+  timeLeft--;
+  if (timeLeft < 0) {
+    clearInterval(timerInterval);
+    document.getElementById("timer").textContent = "Time's up!";
+  }
+}
+
+function startTimer() {
+  duration = document.getElementById("timer-input").value * 60;
+  timeLeft = duration;
+  clearInterval(timerInterval);
+  timerInterval = setInterval(updateTimer, 1000);
+}
+
+document.getElementById("start-button").addEventListener("click", startTimer);
