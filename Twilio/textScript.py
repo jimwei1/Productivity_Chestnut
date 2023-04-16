59851c8614bd3ff7e7d749e0410dd817589d7cac
mschilling314 <mschilling52019@gmail.com>
2023-04-15 20:22:51 -0500

    Twilio Securing Update

--- Twilio/textScript.py
+++ Twilio/textScript.py
@@ -1,11 +1,17 @@
 from twilio.rest import Client
 from .. import constants as c
 import time
+import os
 
 
-def text(msg: str, destination_phone_number: str) -> str:
-    client = Client(c.twilio_account_sid, c.twilio_auth_token)
+def client_initialization():
+    twilio_sid = os.environ.get('twilio_account_sid')
+    twilio_auth_token = os.environ.get('twilio_auth_token')
+    client = Client(twilio_sid, twilio_auth_token)
+    return client
 
+
+def text(client, msg: str, destination_phone_number: str) -> str:
     message = client.messages.create(
         body=msg,
         from_=c.twilio_source_phone_number,
@@ -16,9 +22,7 @@ def text(msg: str, destination_phone_number: str) -> str:
 
 
 
-def call(recording_url: str, destination_phone_number: str) -> str:
-    client = Client(c.twilio_account_sid, c.twilio_auth_token)
-
+def call(client, recording_url: str, destination_phone_number: str) -> str:
     message = client.calls.create(
         url=recording_url,
         from_=c.twilio_source_phone_number,
@@ -31,15 +35,17 @@ def call(recording_url: str, destination_phone_number: str) -> str:
 def alarm_triggered(destination_phone_number: str) -> tuple:
     text_sids = []
     call_sids = []
+    client = client_initialization()
     for i in range(c.twilio_call_limit):
         for j in range(c.twilio_text_limit):
-            text_sids.append(text(c.twilio_text_message, destination_phone_number))
+            text_sids.append(text(client, c.twilio_text_message, destination_phone_number))
             time.sleep(5/(i+1))
-        call_sids.append(call(c.twilio_voice_recording_url, c.twilio_source_phone_number))
+        call_sids.append(call(client, c.twilio_voice_recording_url, c.twilio_source_phone_number))
         time.sleep(10)
     return text_sids, call_sids
 
 
 if __name__=="__main__":
-    sid = text("This is a test", "+1 224 478 5394")
+    client = client_initialization()
+    sid = text(client, "This is a test", "+1 224 478 5394")
     print(sid)
