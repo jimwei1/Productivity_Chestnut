9dc88c4612c982cccda0f71e1dfe455c4bc5ba2a
mschilling314 <mschilling52019@gmail.com>
2023-04-16 05:48:48 -0500

    Update constants.py

--- constants/constants.py
+++ constants/constants.py
@@ -9,3 +9,8 @@ twilio_auth_key_env_name = 'TWILIO_AUTH_TOKEN'
 
 twilio_text_limit = 10
 twilio_call_limit = 3
+
+
+sql_numbers_query = "SELECT NUMBER FROM PEOPLE"
+sql_name_query = ""
+sql_task_query = ""
