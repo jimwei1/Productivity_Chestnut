955b3a397d3e10f5a0056e315aba0118a986abbf
mschilling314 <mschilling52019@gmail.com>
2023-04-16 11:41:39 -0500

    Update textScript.py

--- Twilio/textScript.py
+++ Twilio/textScript.py
@@ -58,7 +58,7 @@ def call(client, recording_url: str, destination_phone_number: str) -> str:
     The SID provided by the client upon sending the message.
     """
     message = client.calls.create(
-        twiml=os.path.join(os.path.dirname(__file__), "./callScript.xml")
+        twiml=os.path.join(os.path.dirname(__file__), "./callScript.xml"),
         from_=c.twilio_source_phone_number,
         to=destination_phone_number
     )
