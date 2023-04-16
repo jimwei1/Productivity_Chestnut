886f57204ed2c334a74eedb8e7a1b93355f6cba4
mschilling314 <mschilling52019@gmail.com>
2023-04-15 23:25:20 -0500

    Update textScript.py

--- Twilio/textScript.py
+++ Twilio/textScript.py
@@ -13,12 +13,12 @@ spec.loader.exec_module(c)
 
 
 def client_initialization():
-    # twilio_sid = os.environ.get('TWILIO_ACCOUNT_SID')
-    # twilio_auth_token = os.environ.get('TWILIO_AUTH_TOKEN')
-    # print(f"\n\n{twilio_sid} {twilio_auth_token}\n\n")
-    twilio_creds = os.environ.get('TWILIO_ENV')
-    print(twilio_creds)
-    client = Client(twilio_creds.sid, twilio_creds.auth_token)
+    twilio_sid = os.environ.get('TWILIO_ACCOUNT_SID')
+    twilio_auth_token = os.environ.get('TWILIO_AUTH_TOKEN')
+    print(f"\n\n{twilio_sid} {twilio_auth_token}\n\n")
+    # twilio_creds = os.environ.get('twilio_env')
+    # print(twilio_creds)
+    client = Client(twilio_sid, twilio_auth_token)
     return client
 
 
