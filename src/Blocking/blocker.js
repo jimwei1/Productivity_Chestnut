6a99ff2025c572c62dfca79b513bfa83984d02f0
Thomas Li <licxb@bc.edu>
2023-04-16 09:22:21 -0500

    update blocker.js

--- src/Blocking/blocker.js
+++ src/Blocking/blocker.js
@@ -1,16 +1,12 @@
-import {constants} from "../constants/constants" 
-const blockList = require('./blockList.json')
+var blockedSites = ["facebook.com", "twitter.com", "instagram.com"];
 
+// Get the current URL
+var currentUrl = window.location.href;
 
-chrome.webRequest.onBeforeRequest.addListener(
-  function(details) {
-    return {
-      redirectUrl: constants.ourWebpage
-    };
-  },
-  {
-    urls: blockList.blocked,
-    types: ['main_frame', 'sub_frame', 'stylesheet', 'script', 'image', 'object', 'xmlhttprequest', 'other'] // might only need main_frame
-  },
-  ['blocking']
-);
+// Check if the current URL matches the list of blocked websites
+for (var i = 0; i < blockedSites.length; i++) {
+  if (currentUrl.indexOf(blockedSites[i]) !== -1) {
+    // Redirect to a custom message page
+    window.location.replace("http://google.com");
+  }
+}
