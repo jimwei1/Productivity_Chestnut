6b23b347ff73976c064f6d1b45a9b24ef035cd3c
a Comfortable Cow <jimwei2022@u.northwestern.edu>
2023-04-15 21:56:13 -0500

    etc

--- src/index.js
+++ src/index.js
@@ -29,8 +29,8 @@ window.onload = function (e) {
         <div id="menu">
           <button @click="showLogin" id="login-button" class="btn">Login</button>
           <button @click="showSignup" id="signup-button" class="btn">Signup</button>
-          <button @click="showProfile" id="profile-button"></button>
-          <button id="settings-button"></button>
+          <button @click="showProfile" id="profile-button" class="btn"></button>
+          <button @click="showSettings" id="settings-button" class="btn"></button>
         </div>
 
         <div id="login-popup" class="popup">
@@ -49,6 +49,41 @@ window.onload = function (e) {
           <button>Signup</button>
         <button class="close-button" @click="hideSignup">Close</button>
       </div>
+
+      <div id="settings-popup" class="popupSettings">
+      <h2>Settings</h2>
+
+
+      <button class="tablink" onclick="openPage('Home', this, 'red')">Home</button>
+      <button class="tablink" onclick="openPage('News', this, 'green')" id="defaultOpen">News</button>
+      <button class="tablink" onclick="openPage('Contact', this, 'blue')">Contact</button>
+      <button class="tablink" onclick="openPage('About', this, 'orange')">About</button>
+      
+      <div id="Home" class="tabcontent">
+        <h3>Home</h3>
+        <p>Home is where the heart is..</p>
+      </div>
+      
+      <div id="News" class="tabcontent">
+        <h3>News</h3>
+        <p>Some news this fine day!</p>
+      </div>
+      
+      <div id="Contact" class="tabcontent">
+        <h3>Contact</h3>
+        <p>Get in touch, or swing by for a cup of coffee.</p>
+      </div>
+      
+      <div id="About" class="tabcontent">
+        <h3>About</h3>
+        <p>Who we are and what we do.</p>
+      </div>
+
+      </div>
+
+
+    </div>
+
       </div>
       <ul id="list">
         <li v-for="item in items"><s>{{ item }}</s></li>
@@ -89,13 +124,7 @@ window.onload = function (e) {
       },
 
       openTimerPopup(){
-        /* hide signup popup */
-        let popupS = document.getElementById("signup-popup");
-        popupS.classList.remove("open-popup");
-
-        /* hide login popup */
-        let popupL = document.getElementById("login-popup");
-        popupL.classList.remove("open-popup");
+        this.hideAllPopups();
 
         /* show timer popup */
         let popup = document.getElementById("timer-popup");
@@ -133,17 +162,11 @@ window.onload = function (e) {
       },
 
       showLogin() {
-        /* hide signup popup */
-        let popupS = document.getElementById("signup-popup");
-        popupS.classList.remove("open-popup");
-
-        /* hide timer popup */
-        let popupT = document.getElementById("timer-popup");
-        popupT.classList.remove("open-popup")
+        this.hideAllPopups();
 
         /* show login popup */
-        let popupL = document.getElementById("login-popup");
-        popupL.classList.add("open-popup");
+        let popup = document.getElementById("login-popup");
+        popup.classList.add("open-popup");
       },
     
       hideLogin() {
@@ -152,6 +175,32 @@ window.onload = function (e) {
       },
     
       showSignup() {
+        this.hideAllPopups();
+
+        /* show signup popup */
+        let popup = document.getElementById("signup-popup");
+        popup.classList.add("open-popup");
+      },
+    
+      hideSignup() {
+        let popup = document.getElementById("signup-popup");
+        popup.classList.remove("open-popup");
+      },
+
+      showSettings() {
+        this.hideAllPopups();
+
+        /* show settings popup */
+        let popup = document.getElementById("settings-popup");
+        popup.classList.add("open-popup");
+      },
+    
+      hideSettings() {
+        let popup = document.getElementById("settings-popup");
+        popup.classList.remove("open-popup");
+      },
+
+      hideAllPopups() {
         /* hide login popup */
         let popupL = document.getElementById("login-popup");
         popupL.classList.remove("open-popup");
@@ -160,15 +209,40 @@ window.onload = function (e) {
         let popupT = document.getElementById("timer-popup");
         popupT.classList.remove("open-popup")
 
-        /* show signup popup */
+        /* hide signup popup */
         let popupS = document.getElementById("signup-popup");
-        popupS.classList.add("open-popup");
+        popupS.classList.remove("open-popup");
+
+        /* hide settings popup */
+        let popupSe = document.getElementById("settings-popup");
+        popupSe.classList.remove("open-popup");
       },
-    
-      hideSignup() {
-        let popup = document.getElementById("signup-popup");
-        popup.classList.remove("open-popup");
+
+      openPage(pageName, elmnt, color) {
+        // Hide all elements with class="tabcontent" by default */
+        var i, tabcontent, tablinks;
+        tabcontent = document.getElementsByClassName("tabcontent");
+        for (i = 0; i < tabcontent.length; i++) {
+          tabcontent[i].style.display = "none";
+        }
+
+        // Remove the background color of all tablinks/buttons
+        tablinks = document.getElementsByClassName("tablink");
+        for (i = 0; i < tablinks.length; i++) {
+          tablinks[i].style.backgroundColor = "";
+        }
+
+        // Show the specific tab content
+        document.getElementById(pageName).style.display = "block";
+
+        // Add the specific color to the button used to open the tab content
+        elmnt.style.backgroundColor = color;
+
+        // Get the element with id="defaultOpen" and click on it
+        document.getElementById("defaultOpen").click(); 
       }
+
+
     },
     computed: {
       timerDisplay() {
