9b1e3ea5b9dd0842c0d8e32889d515b2ceb043e5
a Comfortable Cow <jimwei2022@u.northwestern.edu>
2023-04-15 23:01:41 -0500

    etc

--- src/index.js
+++ src/index.js
@@ -6,90 +6,68 @@ window.onload = function (e) {
   new Vue({
     template: `<div id="app">
     <div>
-      <div id="score">
-        <button @click="blockWebsites">Score</button>
-      </div>
-      <div id="timer-container">
-        <input type="number" v-model="timerValue" min="1" placeholder="Enter time in minutes">
-        <span>{{ timerDisplay }}</span>
-      </div>
+          <div id="score">
+            <button @click="openScore">Score</button>
+          </div>
+          <div id="timer-container">
+            <input type="number" v-model="timerValue" min="1" placeholder="Enter time in minutes">
+            <span>{{ timerDisplay }}</span>
+          </div>
 
+          <div id="container">
+            <button type="submit" class="btn" @click="openTimerPopup">Start Timer</button>
+            <div class="popup" id="timer-popup">
+              <h2>Confirm Start</h2>
+              <button type="submit" class="btn" @click="startTimer">Continue</button>
+              <button type="submit" class="btn" @click="closeTimerPopup">Cancel</button>
+            </div>
+          </div>
 
-      <div id="container">
-        <button type="submit" class="btn" @click="openTimerPopup">Start Timer</button>
-        <div class = "popup" id="timer-popup">
-          <h2>Confirm Start</h2>
-          <button type="submit" class="btn" @click="startTimer">Continue</button>
-          <button type="submit" class="btn" @click="closeTimerPopup">Cancel</button>
+          <div id="container">
+            <div id="menu">
+              <button @click="showLogin" id="login-button" class="btn">Login</button>
+              <button @click="showSignup" id="signup-button" class="btn">Signup</button>
+              <button @click="showProfile" id="profile-button"></button>
+              <button id="settings-button"></button>
+            </div>
+
+            <div id="login-popup" class="popup">
+              <h2>Login</h2>
+              <input type="text" placeholder="Username">
+              <input type="password" placeholder="Password">
+              <button>Login</button>
+              <button class="close-button" @click="hideLogin">Close</button>
+            </div>
+
+            <div id="signup-popup" class="popup">
+              <h2>Signup</h2>
+              <input type="text" placeholder="Username">
+              <input type="password" placeholder="Password">
+              <input type="password" placeholder="Confirm Password">
+              <button>Signup</button>
+              <button class="close-button" @click="hideSignup">Close</button>
+            </div>
+          </div>
+
+          <div id="urls-container" class="urlList">
+          <h2>BlockedUrls</h2>
+            <input id="blockedUrl" type="text" v-model="newUrl" placeholder="Enter URL">
+            <button @click="addUrl">Add URL</button>
+            <ul>
+              <li v-for="url in urls"><s>{{ url }}</s></li>
+            </ul>
+          </div>
+          <div id="task-container" class="taskList">
+          <h2>Tasks</h2>
+            <input id="aTask" type="text" v-model="newTask" placeholder="Enter a Task">
+            <button @click="addTask">Add TASK</button>
+            <ul>
+              <li v-for="task in tasks"><s>{{ task }}</s></li>
+            </ul>
+          </div>
         </div>
-
-
       </div>
-      <div id="container">
-        <div id="menu">
-          <button @click="showLogin" id="login-button" class="btn">Login</button>
-          <button @click="showSignup" id="signup-button" class="btn">Signup</button>
-          <button @click="showProfile" id="profile-button" class="btn"></button>
-          <button @click="showSettings" id="settings-button" class="btn"></button>
-        </div>
-
-        <div id="login-popup" class="popup">
-          <h2>Login</h2>
-          <input type="text" placeholder="Username">
-          <input type="password" placeholder="Password">
-          <button>Login</button>
-          <button class="close-button" @click="hideLogin">Close</button>
-        </div>
-
-        <div id="signup-popup" class="popup">
-          <h2>Signup</h2>
-          <input type="text" placeholder="Username">
-          <input type="password" placeholder="Password">
-          <input type="password" placeholder="Confirm Password">
-          <button>Signup</button>
-        <button class="close-button" @click="hideSignup">Close</button>
-      </div>
-
-      <div id="settings-popup" class="popupSettings">
-      <h2>Settings</h2>
-
-
-      <button class="tablink" onclick="openPage('Home', this, 'red')">Home</button>
-      <button class="tablink" onclick="openPage('News', this, 'green')" id="defaultOpen">News</button>
-      <button class="tablink" onclick="openPage('Contact', this, 'blue')">Contact</button>
-      <button class="tablink" onclick="openPage('About', this, 'orange')">About</button>
-      
-      <div id="Home" class="tabcontent">
-        <h3>Home</h3>
-        <p>Home is where the heart is..</p>
-      </div>
-      
-      <div id="News" class="tabcontent">
-        <h3>News</h3>
-        <p>Some news this fine day!</p>
-      </div>
-      
-      <div id="Contact" class="tabcontent">
-        <h3>Contact</h3>
-        <p>Get in touch, or swing by for a cup of coffee.</p>
-      </div>
-      
-      <div id="About" class="tabcontent">
-        <h3>About</h3>
-        <p>Who we are and what we do.</p>
-      </div>
-
-      </div>
-
-
-    </div>
-
-      </div>
-      <ul id="list">
-        <li v-for="item in items"><s>{{ item }}</s></li>
-      </ul>
-    </div>
-  </div>`,
+    `,
 
     data() {
       return {
@@ -97,15 +75,18 @@ window.onload = function (e) {
         timeLeft: 0,
         timerInterval: null,
         timerValue: null,
-        items: ["Coffee"],
+        newTask: "",
+        tasks: [],
         timerDisplay: "",
         blockListActive: false,
-        audio: null
+        audio: null,
+        newUrl: "",
+        urls: [],
       };
     },
     mounted() {
         // Create the audio element
-        this.audio = new Audio("/path/to/audio/file.mp3");
+        this.audio = new Audio("./sounds/alarm.mp3");
       },
 
     methods: {
@@ -124,7 +105,13 @@ window.onload = function (e) {
       },
 
       openTimerPopup(){
-        this.hideAllPopups();
+        /* hide signup popup */
+        let popupS = document.getElementById("signup-popup");
+        popupS.classList.remove("open-popup");
+
+        /* hide login popup */
+        let popupL = document.getElementById("login-popup");
+        popupL.classList.remove("open-popup");
 
         /* show timer popup */
         let popup = document.getElementById("timer-popup");
@@ -153,7 +140,7 @@ window.onload = function (e) {
 
         this.timerInterval = setInterval(this.updateTimer, 1000);
       },
-      blockWebsites() {
+      openScore() {
         alert("Blocking websites!");
       },
 
@@ -162,11 +149,17 @@ window.onload = function (e) {
       },
 
       showLogin() {
-        this.hideAllPopups();
+        /* hide signup popup */
+        let popupS = document.getElementById("signup-popup");
+        popupS.classList.remove("open-popup");
+
+        /* hide timer popup */
+        let popupT = document.getElementById("timer-popup");
+        popupT.classList.remove("open-popup")
 
         /* show login popup */
-        let popup = document.getElementById("login-popup");
-        popup.classList.add("open-popup");
+        let popupL = document.getElementById("login-popup");
+        popupL.classList.add("open-popup");
       },
     
       hideLogin() {
@@ -175,32 +168,6 @@ window.onload = function (e) {
       },
     
       showSignup() {
-        this.hideAllPopups();
-
-        /* show signup popup */
-        let popup = document.getElementById("signup-popup");
-        popup.classList.add("open-popup");
-      },
-    
-      hideSignup() {
-        let popup = document.getElementById("signup-popup");
-        popup.classList.remove("open-popup");
-      },
-
-      showSettings() {
-        this.hideAllPopups();
-
-        /* show settings popup */
-        let popup = document.getElementById("settings-popup");
-        popup.classList.add("open-popup");
-      },
-    
-      hideSettings() {
-        let popup = document.getElementById("settings-popup");
-        popup.classList.remove("open-popup");
-      },
-
-      hideAllPopups() {
         /* hide login popup */
         let popupL = document.getElementById("login-popup");
         popupL.classList.remove("open-popup");
@@ -209,40 +176,28 @@ window.onload = function (e) {
         let popupT = document.getElementById("timer-popup");
         popupT.classList.remove("open-popup")
 
-        /* hide signup popup */
+        /* show signup popup */
         let popupS = document.getElementById("signup-popup");
-        popupS.classList.remove("open-popup");
-
-        /* hide settings popup */
-        let popupSe = document.getElementById("settings-popup");
-        popupSe.classList.remove("open-popup");
+        popupS.classList.add("open-popup");
       },
-
-      openPage(pageName, elmnt, color) {
-        // Hide all elements with class="tabcontent" by default */
-        var i, tabcontent, tablinks;
-        tabcontent = document.getElementsByClassName("tabcontent");
-        for (i = 0; i < tabcontent.length; i++) {
-          tabcontent[i].style.display = "none";
+    
+      hideSignup() {
+        let popup = document.getElementById("signup-popup");
+        popup.classList.remove("open-popup");
+      },
+      addUrl() {
+        if (this.newUrl != "") {
+          this.urls.push(this.newUrl);
         }
-
-        // Remove the background color of all tablinks/buttons
-        tablinks = document.getElementsByClassName("tablink");
-        for (i = 0; i < tablinks.length; i++) {
-          tablinks[i].style.backgroundColor = "";
+        console.log(this.urls);
+        
+      },
+      addTask() {
+        if (this.newTask != "") {
+          this.tasks.push(this.newTask);
         }
-
-        // Show the specific tab content
-        document.getElementById(pageName).style.display = "block";
-
-        // Add the specific color to the button used to open the tab content
-        elmnt.style.backgroundColor = color;
-
-        // Get the element with id="defaultOpen" and click on it
-        document.getElementById("defaultOpen").click(); 
+        
       }
-
-
     },
     computed: {
       timerDisplay() {
