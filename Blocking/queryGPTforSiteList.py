ad3bbfb75a1eec8ccce997fcb577179239ee4861
mschilling314 <mschilling52019@gmail.com>
2023-04-16 00:30:01 -0500

    Update queryGPTforSiteList.py

--- Blocking/queryGPTforSiteList.py
+++ Blocking/queryGPTforSiteList.py
@@ -6,18 +6,34 @@ import json
 
 
 
-def createGPTMessage(message):
+def createGPTMessage(message: str) -> dict[str, str]:
+    """
+    Creates GPT Message.
+    """
     return {"role": "user", "content": message}
 
 
-def makeGPTRequest(messages):
+def makeGPTRequest(messages: list[str]):
+    """
+    Creates a GPT request.
+    """
     return openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
         messages=messages
     )
 
 
-def createBlockList(categories, recursions=0):
+def createBlockList(categories: str, recursions: int=0) ->:
+    """
+    Queries GPT 3.5 Turbo to get a CSV of URLs of a given category.
+
+    Inputs:
+    categories:  A string that completes the query string: "Please generate a comma-separated list of URLs of the most popular websites in the following categories: {categories}"
+    recursions:  A limit on how many times GPT can be queried if it fails.
+
+    Outputs:
+    No explicit output, it dumps the URL blocklist into blockList.json as a list of URL strings.
+    """
     queryString = f"Please generate a comma-separated list of URLs of the most popular websites in the following categories: {categories}"
     messages = createGPTMessage(queryString)
     response = makeGPTRequest(messages=messages)
